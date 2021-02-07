cc2ce6d2dee09538b2a1c1758513c99d0994a71d
Matyuchshenko Andrej <matvej03111984@gmail.com>
2021-02-07 12:06:40 +0200

    fix hitsApi

--- src/services/hits-api.js
+++ src/services/hits-api.js
@@ -1,28 +1,26 @@
 import axios from "axios";
 
-// const BASE_URL = "https://pixabay.com/api/";
+const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "18815450-007739eb5f1c89a4d6d918d62";
 
-// axios.defaults.baseURL = BASE_URL
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
 
-// axios.defaults.params = {
-//   key: API_KEY,
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   per_page: 12,
-// }
-
-// const apiService = async ({ query, page }) => {
-//   const { data } = await axios.get('', {
-//     params: { query, page },
-//   });
-//   return data.hits
-// };
-const apiService = async (query, page) => {
-  const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-  );
-  return data.hits;
+const apiService = async (q, page) => {
+  try {
+    const { data } = await axios.get('', {
+      params: { q, page },
+    });
+    return data.hits;
+  } catch (error) {
+    console.log('error', { error });
+    return [];
+  }
 };
 
 export default apiService;
